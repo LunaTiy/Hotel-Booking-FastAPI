98616ef9af74529391a414bcc562a6cdbee7a1c8
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-23 18:34:27 +0300

    chore: add requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+﻿from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="hotel-booking-fastapi",
+    packages=find_packages(),
+    python_requires=">=3.11.3",
+    install_requires=requirements
+)
