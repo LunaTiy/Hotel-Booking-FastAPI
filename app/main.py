9469125a7c6a6f3ae1f6efdc2e9fa23ad203b44b
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-23 21:13:46 +0300

    feat: add simple post request

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 from typing import Optional
 
 from fastapi import FastAPI, Query
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -15,3 +16,14 @@ def get_hotels(
         stars: Optional[int] = Query(None, ge=1, le=5)
 ):
     return location, date_from, date_to, has_spa, stars
+
+
+class SchemaBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/bookings")
+def add_booking(booking: SchemaBooking):
+    pass
