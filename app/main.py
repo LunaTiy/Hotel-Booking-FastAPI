3e97180bd17d8d13a9a4c7ed7d70709ef6d8be58
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-23 20:35:59 +0300

    feat: add base type validation for hotels endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
-﻿from fastapi import FastAPI
+﻿from datetime import date
+from typing import Optional
+
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
 @app.get("/hotels")
-def get_hotels():
-    return "Отель Лагуна Бридж 5*"
+def get_hotels(
+        location: str,
+        date_from: date,
+        date_to: date,
+        has_spa: Optional[bool] = None,
+        stars: Optional[int] = Query(None, ge=1, le=5)
+):
+    return location, date_from, date_to, has_spa, stars
