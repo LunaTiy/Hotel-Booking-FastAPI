b4501b06aece8f0a54e876e67cc7c877706d242f
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-24 11:34:50 +0300

    feat: add response typing and move request params to class

--- app/main.py
+++ app/main.py
@@ -1,21 +1,42 @@
 ﻿from datetime import date
-from typing import Optional
+from typing import Optional, List
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class HotelSearchArgs:
+    def __init__(
+            self,
+            location: str,
+            date_from: date,
+            date_to: date,
+            has_spa: Optional[bool] = None,
+            stars: Optional[int] = Query(None, ge=1, le=5)
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
+class SchemaHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+
 @app.get("/hotels")
 def get_hotels(
-        location: str,
-        date_from: date,
-        date_to: date,
-        has_spa: Optional[bool] = None,
-        stars: Optional[int] = Query(None, ge=1, le=5)
-):
-    return location, date_from, date_to, has_spa, stars
+        search_args: HotelSearchArgs = Depends()
+) -> List[SchemaHotel]:
+    hotels = [
+        SchemaHotel(address="New-York city, green street, house 5", name="Great hotel", stars=4)
+    ]
+    return hotels
 
 
 class SchemaBooking(BaseModel):
