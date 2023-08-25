65853af12b40e1ea3dfdac4f333ed55718e005bc
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-25 17:22:36 +0300

    feat: add booking model, add revision, upgrade db

---
+++ app/bookings/models.py
@@ -0,0 +1,19 @@
+﻿from datetime import date
+
+from sqlalchemy import Integer, ForeignKey, Date, Computed
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Booking(Base):
+    __tablename__ = "bookings"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    date_from: Mapped[date] = mapped_column(Date, nullable=False)
+    date_to: Mapped[date] = mapped_column(Date, nullable=False)
+    price: Mapped[int] = mapped_column(Integer, nullable=False)
+    total_cost: Mapped[int] = mapped_column(Integer, Computed("(date_to - date_from) * price"), nullable=False)
+    total_days: Mapped[int] = mapped_column(Integer, Computed("date_to - date_from"), nullable=False)
--- app/migrations/env.py
+++ app/migrations/env.py
@@ -9,6 +9,7 @@ from app.database import Base
 from app.hotels.models import Hotel
 from app.hotels.rooms.models import Room
 from app.users.models import User
+from app.bookings.models import Booking
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
