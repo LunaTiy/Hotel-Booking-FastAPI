58c888550e0e7ce052ddd96a877f3fe3958f7716
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-25 16:22:41 +0300

    feat: add .env file with private data (it was added to git,so it's pet project)

---
+++ app/config.py
@@ -0,0 +1,23 @@
+﻿from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DMS: str
+    DMS_DRIVER: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def database_url(self):
+        driver = f'{self.DMS}+{self.DMS_DRIVER}'
+        user = f"{self.DB_USER}:{self.DB_PASS}"
+        database = f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"{driver}://{user}@{database}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
--- app/database.py
+++ app/database.py
@@ -1,17 +1,9 @@
-﻿import os
-
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+﻿from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", 5432)
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASS = os.getenv("DB_PASS", "postgres")
-DB_NAME = os.getenv("DB_NAME", "hotel-booking")
+from config import settings
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(settings.database_url)
 async_session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
