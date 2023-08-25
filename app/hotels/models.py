45503464e51ce485d58deec8513f7da603fd3a80
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-25 13:21:15 +0300

    feat: create hotel model

---
+++ app/hotels/models.py
@@ -0,0 +1,17 @@
+﻿from typing import Optional
+
+from sqlalchemy import Integer, String, JSON, SmallInteger
+from sqlalchemy.orm import mapped_column, Mapped
+
+from app.database import Base
+
+
+class Hotel(Base):
+    __tablename__ = "hotels"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(128), nullable=False)
+    location: Mapped[str] = mapped_column(String(255), nullable=False)
+    services: Mapped[Optional[str]] = mapped_column(JSON)
+    rooms_quantity: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    image_id: Mapped[int] = mapped_column(Integer)
