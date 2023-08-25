5c0068d924ac0bda70a3227173588558fdc86962
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-25 12:13:29 +0300

    feat: add reading db connection params from env

--- app/database.py
+++ app/database.py
@@ -1,11 +1,13 @@
-﻿from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+﻿import os
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-DB_HOST = "localhost"
-DB_PORT = 5432
-DB_USER = "postgres"
-DB_PASS = "postgres"
-DB_NAME = "hotel-booking"
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", 5432)
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASS = os.getenv("DB_PASS", "postgres")
+DB_NAME = os.getenv("DB_NAME", "hotel-booking")
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
