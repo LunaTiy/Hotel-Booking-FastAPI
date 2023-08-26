97eb4eeaa6e68ed08687306214630c32ce095996
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-26 12:45:56 +0300

    refactor: add type annotation in settings class

--- app/config.py
+++ app/config.py
@@ -1,17 +1,19 @@
-﻿from pydantic_settings import BaseSettings, SettingsConfigDict
+﻿from typing import Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DMS: str
-    DMS_DRIVER: str
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
+    DMS: Optional[str] = None
+    DMS_DRIVER: Optional[str] = None
+    DB_HOST: Optional[str] = None
+    DB_PORT: Optional[int] = None
+    DB_USER: Optional[str] = None
+    DB_PASS: Optional[str] = None
+    DB_NAME: Optional[str] = None
 
     @property
-    def database_url(self):
+    def database_url(self) -> str:
         driver = f'{self.DMS}+{self.DMS_DRIVER}'
         user = f"{self.DB_USER}:{self.DB_PASS}"
         database = f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
