490674014b9978d297a32185eb884d099ad6df97
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-26 19:45:38 +0300

    feat: add base query

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,4 +1,8 @@
 ﻿from fastapi import APIRouter
+from sqlalchemy import select
+
+from app.bookings.models import Booking
+from app.database import async_session_maker
 
 router = APIRouter(
     prefix="/bookings",
@@ -7,8 +11,11 @@ router = APIRouter(
 
 
 @router.get("")
-def get_bookings():
-    pass
+async def get_bookings():
+    async with async_session_maker() as session:
+        query = select(Booking)
+        result = await session.execute(query)
+        return result.mappings().all()
 
 
 @router.get("/{booking_id}")
