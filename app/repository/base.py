b238d97b0b12f9a57f19cb4c7f5b43eeb58ee0f9
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-28 21:51:55 +0300

    feat: add filter in db query, add method find_one in BaseRepository

--- app/repository/base.py
+++ app/repository/base.py
@@ -7,8 +7,15 @@ class BaseRepository:
     model = None
 
     @classmethod
-    async def find_all(cls):
+    async def find_one_or_none(cls, *filter_by):
         async with async_session_maker() as session:
-            query = select(cls.model)
+            query = select(cls.model.__table__.columns).filter(*filter_by)
+            result = await session.execute(query)
+            return result.mappings().one_or_none()
+
+    @classmethod
+    async def find_all(cls, *filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model.__table__.columns).filter(*filter_by)
             result = await session.execute(query)
             return result.mappings().all()
