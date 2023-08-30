fb2367b1f5058692ed0afb749b660369d9074cf2
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-30 16:16:49 +0300

    refactor: parse user id in jwt token to string, add empty lines

--- app/users/router.py
+++ app/users/router.py
@@ -25,8 +25,10 @@ async def register_user(user_data: SchemeUserAuth):
 @router.post("/login")
 async def login_user(response: Response, user_data: SchemeUserAuth):
     user = await authenticate_user(user_data.email, user_data.password)
+
     if not user:
         raise HTTPException(status.HTTP_401_UNAUTHORIZED)
-    access_token = create_access_token({"sub": user.id})
+
+    access_token = create_access_token({"sub": str(user.id)})
     response.set_cookie("booking_access_token", access_token, httponly=True)
     return access_token
