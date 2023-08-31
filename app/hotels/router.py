55bd16a123e5c64e058f0c7b6aff79adc44973c0
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-08-31 17:42:48 +0300

    feat: add schema hotel and endpoint for getting info about hotel by id

--- app/hotels/router.py
+++ app/hotels/router.py
@@ -1,9 +1,11 @@
 ﻿from datetime import date
-from typing import List
+from typing import List, Optional
 
 from fastapi import APIRouter
 
-from app.hotels.schemas import SchemaAvailableHotel
+from app.hotels.models import Hotel
+from app.hotels.repsitory import HotelRepository
+from app.hotels.schemas import SchemaAvailableHotel, SchemaHotel
 from app.hotels.service import get_available_hotels
 
 router = APIRouter(
@@ -15,3 +17,8 @@ router = APIRouter(
 @router.get("/{location}")
 async def get_hotels(location: str, date_from: date, date_to: date) -> List[SchemaAvailableHotel]:
     return await get_available_hotels(location, date_from, date_to)
+
+
+@router.get("/id/{hotel_id}")
+async def get_hotel_by_id(hotel_id: int) -> Optional[SchemaHotel]:
+    return await HotelRepository.find_one_or_none(Hotel.id == hotel_id)
--- app/hotels/schemas.py
+++ app/hotels/schemas.py
@@ -3,6 +3,15 @@
 from pydantic import BaseModel
 
 
+class SchemaHotel(BaseModel):
+    id: int
+    name: str
+    location: str
+    services: Optional[List[str]]
+    rooms_quantity: int
+    image_id: Optional[int]
+
+
 class SchemaAvailableHotel(BaseModel):
     id: int
     name: str
