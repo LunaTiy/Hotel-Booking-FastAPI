8535d58dd4c1cb0fc4b514bfcd1fa423048ba5ae
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-09-13 21:47:07 +0300

    feat: add base template for render hotels

--- app/pages/router.py
+++ app/pages/router.py
@@ -3,7 +3,7 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 
-from app.hotels.router import get_hotels
+from app.hotels.router import get_hotels_by_location_and_time
 from app.hotels.schemas import SchemaAvailableHotel
 
 router = APIRouter(
@@ -17,6 +17,9 @@ templates = Jinja2Templates(directory="app/templates")
 @router.get("/hotels")
 async def get_hotels_page(
         request: Request,
-        hotels: List[SchemaAvailableHotel] = Depends(get_hotels)
+        hotels: List[SchemaAvailableHotel] = Depends(get_hotels_by_location_and_time)
 ):
-    return templates.TemplateResponse(name="hotels.html", context={"request": request})
+    return templates.TemplateResponse(
+        name="hotels.html",
+        context={"request": request, "hotels": hotels}
+    )
