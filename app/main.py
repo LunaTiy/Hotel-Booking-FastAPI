9ed0e7063049b94e2be96bb8a88c463429c1e5c1
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-09-15 15:51:26 +0300

    feat: add images router, add endpoint for upload files

--- app/main.py
+++ app/main.py
@@ -1,50 +1,21 @@
-﻿from datetime import date
-from typing import Optional, List
-
-from fastapi import FastAPI, Query, Depends
-from pydantic import BaseModel
+﻿from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.bookings.router import router as bookings_router
 from app.hotels.rooms.router import router as rooms_router
 from app.hotels.router import router as hotels_router
+from app.images.router import router as images_router
+from app.pages.router import router as pages_router
 from app.users.router import router as users_router
 
-from app.pages.router import router as pages_router
-
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), "static")
+
 app.include_router(users_router)
 app.include_router(hotels_router)
 app.include_router(rooms_router)
 app.include_router(bookings_router)
 
 app.include_router(pages_router)
-
-
-class HotelSearchArgs:
-    def __init__(
-            self,
-            location: str,
-            date_from: date,
-            date_to: date,
-            has_spa: Optional[bool] = None,
-            stars: Optional[int] = Query(None, ge=1, le=5)
-    ):
-        self.location = location
-        self.date_from = date_from
-        self.date_to = date_to
-        self.has_spa = has_spa
-        self.stars = stars
-
-
-class SchemaHotel(BaseModel):
-    address: str
-    name: str
-    stars: int
-
-
-@app.get("/hotels")
-def get_hotels(search_args: HotelSearchArgs = Depends()) -> List[SchemaHotel]:
-    hotels = [
-        SchemaHotel(address="New-York city, green street, house 5", name="Great hotel", stars=4)
-    ]
-    return hotels
+app.include_router(images_router)
