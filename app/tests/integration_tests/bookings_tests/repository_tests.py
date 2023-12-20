abaf29d8d4c7566d02c9b438a001bd9e6c7282f1
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-12-20 13:47:53 +0300

    test: add integration tests booking repository

---
+++ app/tests/integration_tests/bookings_tests/repository_tests.py
@@ -0,0 +1,29 @@
+﻿from datetime import date
+
+import pytest
+
+from app.bookings.models import Booking
+from app.bookings.repository import BookingRepository
+
+
+@pytest.mark.parametrize(
+    ("user_id", "room_id", "date_from", "date_to"),
+    [
+        (2, 2, date(2023, 7, 10), date(2023, 7, 24)),
+        (1, 3, date(2023, 10, 15), date(2023, 10, 29)),
+    ]
+)
+async def test_add_and_get_booking(
+        user_id: int,
+        room_id: int,
+        date_from: date,
+        date_to: date
+) -> None:
+    new_booking = await BookingRepository.add(user_id, room_id, date_from, date_to)
+
+    assert new_booking.user_id == user_id
+    assert new_booking.room_id == room_id
+
+    new_booking = await BookingRepository.find_one_or_none(Booking.id == new_booking.id)
+
+    assert new_booking
