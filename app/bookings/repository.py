5e9bb10d9b4f008c08deb2cc7ef28326cac12220
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2024-01-07 20:43:24 +0300

    refactor: apply black fixes for bookings repository

--- app/bookings/repository.py
+++ app/bookings/repository.py
@@ -12,17 +12,32 @@ class BookingRepository(BaseRepository[Booking]):
     model = Booking
 
     @classmethod
-    async def get_count_available_rooms(cls, room_id: int, date_from: date, date_to: date) -> int:
-        booked_rooms = select(cls.model).where(
-            (Booking.room_id == room_id) &
-            ((Booking.date_from <= date_to) & (Booking.date_to >= date_from))
-        ).cte("booked_rooms")
+    async def get_count_available_rooms(
+        cls,
+        room_id: int,
+        date_from: date,
+        date_to: date
+    ) -> int:
+        booked_rooms = (
+            select(cls.model)
+            .where(
+                (Booking.room_id == room_id)
+                & ((Booking.date_from <= date_to) & (Booking.date_to >= date_from))
+            )
+            .cte("booked_rooms")
+        )
 
-        get_rooms_left = select(
-            (Room.quantity - func.count(booked_rooms.columns.room_id)).label("rooms_left")
-        ).select_from(Room).join(
-            booked_rooms, booked_rooms.columns.room_id == Room.id, isouter=True
-        ).where(Room.id == room_id).group_by(Room.quantity, booked_rooms.columns.room_id)
+        get_rooms_left = (
+            select(
+                (Room.quantity - func.count(booked_rooms.columns.room_id)).label(
+                    "rooms_left"
+                )
+            )
+            .select_from(Room)
+            .join(booked_rooms, booked_rooms.columns.room_id == Room.id, isouter=True)
+            .where(Room.id == room_id)
+            .group_by(Room.quantity, booked_rooms.columns.room_id)
+        )
 
         async with async_session_maker() as session:
             rooms_left_result = await session.execute(get_rooms_left)
@@ -30,7 +45,13 @@ class BookingRepository(BaseRepository[Booking]):
             return rooms_left
 
     @classmethod
-    async def add(cls, user_id: int, room_id: int, date_from: date, date_to: date) -> model | None:
+    async def add(
+        cls,
+        user_id: int,
+        room_id: int,
+        date_from: date,
+        date_to: date
+    ) -> model | None:
         """
         -- Заезд '2023-05-15'
         -- Выезд '2023-06-20'
@@ -47,8 +68,7 @@ class BookingRepository(BaseRepository[Booking]):
         WHERE rooms.id = 1
         GROUP BY rooms.quantity, booked_rooms.room_id
         """
-        rooms_left = await cls.get_count_available_rooms(room_id, date_from, date_to
-                                                         )
+        rooms_left = await cls.get_count_available_rooms(room_id, date_from, date_to)
         if rooms_left <= 0:
             return None
 
@@ -58,13 +78,17 @@ class BookingRepository(BaseRepository[Booking]):
 
             current_price: int = price.mappings().one().price
 
-            add_booking = insert(cls.model).values(
-                room_id=room_id,
-                user_id=user_id,
-                date_from=date_from,
-                date_to=date_to,
-                price=current_price,
-            ).returning(Booking.__table__.columns)
+            add_booking = (
+                insert(cls.model)
+                .values(
+                    room_id=room_id,
+                    user_id=user_id,
+                    date_from=date_from,
+                    date_to=date_to,
+                    price=current_price,
+                )
+                .returning(Booking.__table__.columns)
+            )
 
             new_booking = await session.execute(add_booking)
             await session.commit()
--- app/tasks/scheduled_tasks.py
+++ app/tasks/scheduled_tasks.py
@@ -3,4 +3,4 @@
 
 @celery_app.task(name="periodic_task")
 def periodic_task() -> None:
-    print(12345)
+    print(12345)  # noqa: T201
